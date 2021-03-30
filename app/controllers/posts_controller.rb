c8087418ba12e3488ba1e08e45b35278d5513e5f
JuzerShakir <juz.shakir.software@gmail.com>
2021-03-30 20:48:39 +0530

    Show user posts on homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,10 @@
 class PostsController < ApplicationController
+
   def create
     Post.create(post_params)
     redirect_to root_path
   end
 
-  validate :image_presence
-
-  def image_presence
-    errors.add(:image, "can't be blank") unless image.attached?
-  end
   private
     def post_params
       params.require(:post).permit(:description, :user_id, :image)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  validate :image_presence
+
+  def image_presence
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
+
   # => with this, we have connected active_storage to Post model
   # active_storage stores images
   # we can access the image of a post by post.image
