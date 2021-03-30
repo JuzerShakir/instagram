b9b878c85ba4b2c5d69f6e567fed125df40af7c6
JuzerShakir <juz.shakir.software@gmail.com>
2021-03-30 17:33:21 +0530

    Initiate Active Storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  # => with this, we have connected active_storage to Post model
+  # active_storage stores images
+  # we can access the image of a post by post.image
+  has_one_attached :image
 end
